a980d62d0ad5708c392afbfadf82a4791bbb9d2e
isisLuinoGitHub <lagonigro.giuseppe@isisluino.net>
2023-05-29 09:32:46 +0200

    INIT

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/Gruntfile.js to edit this template
+ */
+module.exports = function (grunt)
+{
+    // Project configuration.
+    grunt.initConfig({
+    });
+};
